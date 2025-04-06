358a210dad3eeec8abb55dd5359fa6b84dc019e6
José Messias <messias.bandeira65@gmail.com>
2025-04-05 21:30:13 -0300

    commit 2 - nova biblioteca para trabalhar com templates de documentos e possível necessidade de usar um servidor

--- main.js
+++ main.js
@@ -1,7 +1,51 @@
+const Docxtemplater = require("docxtemplater");
+const PizZip = require("pizzip");
+const fs = require("fs");
+
+const path = require("path");
+
 var nome = document.getElementById('name').value;
 var cpf = document.getElementById('cpf').value;
 
 function gerar(){
+    const content = fs.readFileSync(
+        path.resolve(__dirname, "templatedoc/input.docx"),
+        "binary"
+    );
+    
+    const zip = new PizZip(content);
+
+
+
+
+    const doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+    });
+
+
+    
+
+    doc.render({
+        nome: nome,
+        cpf: cpf
+/*         phone: "+33666666",
+        description: "The Acme Product", */
+    });
+    
+    
+
+
+
+
+    const buf = doc.getZip().generate({
+        type: "nodebuffer",
+        compression: "DEFLATE",
+    });
+    
+
+    fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
+    
     
 };
 
